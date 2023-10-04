7369c0e767e4fa3f60a890e9d27f589211de23dd
unknown <95885719+fotmjay@users.noreply.github.com>
2023-10-04 13:50:15 -0400

    added full wallet amount vs value and % arrow in fullwalletmodal

--- src/components/dashboardComponents/FullWalletModal.tsx
+++ src/components/dashboardComponents/FullWalletModal.tsx
@@ -4,6 +4,8 @@ import { tableConfig } from "../../constants/tableConfig";
 import { Token } from "../../shared/types/types";
 import TableTitles from "./TableComponents/TableTitles";
 import { useRef } from "react";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 type Props = {
   savedTokenList: Token[];
@@ -14,8 +16,8 @@ type Props = {
 export default function FullWalletModal(props: Props) {
   const mediaSmall = useMediaQuery("(max-width:695px)");
   const modalWidth = mediaSmall ? "90%" : "625px"; // 625px is max size as 90% of 695px is ~625px
-  const totalPaid = useRef(0);
-  const totalValue = useRef(0);
+  const totalPaid = useRef<number>(0);
+  const totalValue = useRef<number>(0);
 
   const style = {
     position: "absolute" as "absolute",
@@ -38,26 +40,34 @@ export default function FullWalletModal(props: Props) {
       if (!isNaN(tokenAmount) && tokenAmount !== 0) {
         const updatedToken = { ...token, total: "0", paid: "0", color: "" };
         updatedToken.total = (parseFloat(token.amount) * parseFloat(token.usd)).toString();
-        totalValue.current += +updatedToken.total;
+        totalValue.current! += +updatedToken.total;
         updatedToken.paid = (parseFloat(token.amount) * parseFloat(token.averagePrice)).toFixed(0);
-        totalPaid.current += +updatedToken.paid;
+        totalPaid.current! += +updatedToken.paid;
         updatedToken.color = updatedToken.paid > updatedToken.total ? "red" : "lightgreen";
         filteredListWithTotals.push(updatedToken);
       }
     });
     return filteredListWithTotals.sort((a, b) => b.total - a.total);
   }
+
+  const tokenHeldList = prepareTokenList();
+  const positiveReturns = totalPaid.current > totalValue.current;
   return (
     <Modal open={props.open} onClose={() => props.closeModal()} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
       <Box sx={style}>
         <Typography gutterBottom variant="h4" textAlign="center">
           Wallet details
         </Typography>
-        <Box maxWidth="175px" display="flex" flexWrap="wrap" alignItems="space-between" justifyContent="flex-end">
-          <Typography>Total Paid: {totalPaid.current.toFixed(0)}</Typography>
-          <Typography gutterBottom>Current Value: {totalValue.current.toFixed(0)}</Typography>
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Box maxWidth="175px" display="flex" flexWrap="wrap" alignItems="space-between" justifyContent="flex-end">
+            <Typography>Total Paid: {totalPaid.current.toFixed(0)}$</Typography>
+            <Typography gutterBottom>Current Value: {totalValue.current!.toFixed(0)}$</Typography>
+          </Box>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Typography>{((totalValue.current / totalPaid.current - 1) * 100).toFixed(2)}%</Typography>
+            {positiveReturns ? <ArrowDownwardIcon fontSize="large" /> : <ArrowUpwardIcon fontSize="large" />}
+          </Box>
         </Box>
-
         <TableContainer sx={{ position: "relative" }} component={Paper}>
           <Table align="center" size="small" aria-label="simple table">
             {props.savedTokenList.length > 0 && (
@@ -65,7 +75,7 @@ export default function FullWalletModal(props: Props) {
                 <TableTitles categories={tableConfig.walletTotalCategories} />
               </TableHead>
             )}
-            <TableRows namedProperties={tableConfig.walletTotalCategories.apiRelated} dataList={prepareTokenList()} />
+            <TableRows namedProperties={tableConfig.walletTotalCategories.apiRelated} dataList={tokenHeldList} />
           </Table>
         </TableContainer>
       </Box>
